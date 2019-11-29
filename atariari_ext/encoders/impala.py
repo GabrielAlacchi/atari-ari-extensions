502aa368ebf3d8340461df2fc0dd4093d2b6f1bf
Gabriel Alacchi <alacchi.g@gmail.com>
2019-11-29 14:55:28 -0500

    Add fmaps to ImpalaCNN encoder

--- atariari_ext/encoders/impala.py
+++ atariari_ext/encoders/impala.py
@@ -1,7 +1,21 @@
 import atariari.methods.encoders as encoders
+import torch.nn.functional as F
 
 
 class ImpalaCNN(encoders.ImpalaCNN):
 
-	def forward(self, inputs, fmaps=False):
-		return super().forward(inputs)
+    def forward(self, inputs, fmaps=False):
+        f5 = self.layer3(self.layer2(self.layer1(inputs)))
+
+        if not self.downsample:
+            f5 = self.layer4(f5)
+
+        out = F.relu(self.final_linear(self.flatten(f5)))
+
+        if fmaps:
+            return {
+                "f5": f5.permute(0, 2, 3, 1),
+                "out": out
+            }
+
+        return out
