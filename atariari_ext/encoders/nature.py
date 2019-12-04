442b59ff11ea213b9d30920f892809880e803f14
Gabriel Alacchi <alacchi.g@gmail.com>
2019-12-04 14:05:19 -0500

    Add selective receptive field nature CNN

--- atariari_ext/encoders/nature.py
+++ atariari_ext/encoders/nature.py
@@ -1,8 +1,31 @@
 import atariari.methods.encoders as encoders
 
 
-class NatureCNN(encoders.NatureCNN):
+class SelectiveReceptiveFieldNatureCNN(encoders.NatureCNN):
 
-	@property
-	def convolution_depth(self):
-		return self.main[4].out_channels
+    def __init__(self, input_channels, args, receptive_field=16):
+        super().__init__(input_channels, args)
+
+        if receptive_field not in [4, 8, 16]:
+            raise ValueError("Invalid receptive field, must be one of [4, 8, 16]")
+
+        self.local_layer = 2 * [4, 8, 16].index(receptive_field)
+
+    @property
+    def local_layer_depth(self):
+        return self.main[self.local_layer].out_channels
+
+    def forward(self, inputs, fmaps=False):
+        f5 = self.main[:self.local_layer + 2](inputs)
+        out = self.main[self.local_layer + 2:](f5)
+
+        if self.end_with_relu:
+            assert self.args.method != "vae", "can't end with relu and use vae!"
+            out = F.relu(out)
+
+        if fmaps:
+            return {
+                'f5': f5.permute(0, 2, 3, 1),
+                'out': out
+            }
+        return out
--- train_encoder.py
+++ train_encoder.py
@@ -11,15 +11,16 @@ import torch
 
 from atariari.methods.dim_baseline import DIMTrainer
 from atariari.methods.utils import get_argparser
-from atariari_ext.encoders.nature import NatureCNN
-from atariari_ext.encoders.impala import ImpalaCNN
-from atariari_ext.trainers.stdim import InfoNCESpatioTemporalTrainer
+from atariari.methods.encoders.nature import NatureCNN, ImpalaCNN
+from atariari.methods.stdim import InfoNCESpatioTemporalTrainer
 from atariari.benchmark.episodes import get_episodes
+from atariari_ext.encoders.nature import SelectiveReceptiveFieldNatureCNN
 
 
 parser = get_argparser()
 parser.add_argument('--entity', default='neurips-challenge', dest='entity')
 parser.add_argument('--project', default='atari-ari', dest='project')
+parser.add_argument('--receptive-field', default=16, type=int, dest='receptive_field')
 args = parser.parse_args(sys.argv[1:])
 
 device = torch.device("cuda:" + str(args.cuda_id) if torch.cuda.is_available() else "cpu")
@@ -39,8 +40,11 @@ tr_eps, val_eps = get_episodes(steps=args.pretraining_steps,
 observation_shape = tr_eps[0][0].shape
 if args.encoder_type == "Nature":
     encoder = NatureCNN(observation_shape[0], args)
+elif args.encoder_type == "ReceptiveNature":
+    encoder = SelectiveReceptiveFieldNatureCNN(observation_shape[0], args, receptive_field=args.receptive_field)
 elif args.encoder_type == "Impala":
     encoder = ImpalaCNN(observation_shape[0], args)
+
 encoder.to(device)
 torch.set_num_threads(1)
 
